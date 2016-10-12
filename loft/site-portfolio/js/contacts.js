db2d4957857581ce163fbd80ec2e63d6f8421261
Peccansy <gotodno@gmail.com>
2016-10-12 23:20:59 +0300

    123

--- loft/site-portfolio/js/contacts.js
+++ loft/site-portfolio/js/contacts.js
@@ -1,16 +1,4 @@
 $(document).ready(function() {
-	var target=$('.contacts__trigger');
-	var contacts=$('.contacts__dropdown');	
-	target.click(function(){
-		target=$(this);
-		if(!target.is('.contacts__trigger_active')){
-			target.addClass('contacts__trigger_active');
-			contacts.addClass('contacts__dropdown_triggered');
-		}else{
-			target.removeClass('contacts__trigger_active');
-			contacts.removeClass('contacts__dropdown_triggered');
-		}
-	});
 
 	function windowSize() {
 		if ($(window).width()<=480){
